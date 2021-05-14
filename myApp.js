b79befa7d81d2ede22fccc74dfdb3af40781e5d1
Meet Bhanderi <meettechnical8966@gmail.com>
2021-05-14 22:14:45 +0530

    added hsts

--- myApp.js
+++ myApp.js
@@ -8,6 +8,7 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge: 7776000, force: true}));
 
 
 
